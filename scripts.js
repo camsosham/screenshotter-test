75fb467a794ae811666a10c0184496c62163a2b9
camsosham <camsosham@gmail.com>
2025-12-11 00:25:53 -0600

    SB Connection + Status Indicator

---
+++ scripts.js
@@ -0,0 +1,49 @@
+////////////////////////
+//CONNECT STREAMER.BOT//
+////////////////////////
+
+// Parse URL Parameters
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const sbServerAddress = urlParams.get("address") || "127.0.0.1";
+const sbServerPort = urlParams.get("port") || "8080";
+const endScene = urlParams.get("scene") || "/";
+const overlay = urlParams.get("overlay") || "";
+
+// Create Streamer.bot Client
+const client = new StreamerbotClient({
+	host: sbServerAddress,
+	port: sbServerPort,
+
+	onConnect: (data) => {
+		console.log(`Streamer.bot successfully connected to ${sbServerAddress}:${sbServerPort}`)
+		console.debug(data);
+		SetConnectionStatus(true);
+	},
+
+	onDisconnect: () => {
+		console.error(`Streamer.bot disconnected from ${sbServerAddress}:${sbServerPort}`)
+		SetConnectionStatus(false);
+	}
+});
+
+// Streamer.bot Connection Status Indicator Function
+function SetConnectionStatus(connected) {
+	let statusContainer = document.getElementById("statusContainer");
+	if (connected) {
+		statusContainer.style.background = "#2FB774";
+		statusContainer.innerText = "Connected!";
+		statusContainer.style.opacity = 1;
+		setTimeout(() => {
+			statusContainer.style.transition = "all 2s ease";
+			statusContainer.style.opacity = 0;
+		}, 10);
+	}
+	else {
+		statusContainer.style.background = "#D12025";
+		statusContainer.innerText = "Connecting...";
+		statusContainer.style.transition = "";
+		statusContainer.style.opacity = 1;
+    }
+}
